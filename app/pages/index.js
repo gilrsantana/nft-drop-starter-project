3d2cade748b18909f8e0dfcfaa4b583a567f4266
gilrsantana <g.r.santana@hotmail.com>
2023-05-26 02:39:56 -0300

    creating assets

--- app/pages/index.js
+++ app/pages/index.js
@@ -1,35 +1,51 @@
-import React from "react"
-import dynamic from "next/dynamic"
+import React from "react";
+import { useWallet } from "@solana/wallet-adapter-react";
+import dynamic from "next/dynamic";
 
 // Constants
-const TWITTER_HANDLE = "web3dev_"
-const TWITTER_LINK = `https://twitter.com/${TWITTER_HANDLE}`
+const TWITTER_HANDLE = "web3dev_";
+const TWITTER_LINK = `https://twitter.com/${TWITTER_HANDLE}`;
 
 const Home = () => {
   const WalletMultiButtonDynamic = dynamic(
-    async () => (await import("@solana/wallet-adapter-react-ui")).WalletMultiButton,
+    async () =>
+      (await import("@solana/wallet-adapter-react-ui")).WalletMultiButton,
     { ssr: false }
-  )
+  );
+  const wallet = useWallet();
+
+  // Ações
+  const renderNotConnectedContainer = () => (
+    <div>
+      <img
+        src="https://media.giphy.com/media/eSwGh3YK54JKU/giphy.gif"
+        alt="emoji"
+      />
+
+      <div className="button-container">
+        <WalletMultiButtonDynamic className="cta-button connect-wallet-button" />
+      </div>
+    </div>
+  );
 
   return (
     <div className="App">
-      <div className="container">
-        <div className="header-container">
-          <p className="header">🍭 Máquina de Doces</p>
-          <p className="sub-text">Máquina de doces para mintar NFTs</p>
-        </div>
-        <div className="footer-container">
-          <img alt="Twitter Logo" className="twitter-logo" src="twitter-logo.svg" />
-          <a
-            className="footer-text"
-            href={TWITTER_LINK}
-            target="_blank"
-            rel="noreferrer"
-          >{`built on @${TWITTER_HANDLE}`}</a>
-        </div>
-      </div>
-    </div>
-  )
-}
+        <div className="container">
+            <div className="header-container">
+                <p className="header">🍭 Candy Drop</p>
+                <p className="sub-text">Máquina de NFTs com cunhagem justa</p>
+                {/* Renderize seu botão "Conectar à carteira" aqui */}
+                {wallet.publicKey ? wallet.publicKey.toString() : renderNotConnectedContainer()}
+            </div>
 
-export default Home
+            <div className="footer-container">
+                <img alt="Twitter Logo" className="twitter-logo" src="twitter-logo.svg" />
+                <a className="footer-text" href={TWITTER_LINK} target="_blank" rel="noreferrer">{`Construído no @${TWITTER_HANDLE}`}</a>
+            </div>
+        </div>
+    </div>
+);
+
+};
+
+export default Home;
